6625cc766323ec321f9ea50a5c13f45097aef838
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-07-16 19:03:54 +0545

    Practice makes us perfect

---
+++ CP/PRACTICEPERF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class PRACTICEPERF
+{
+
+    public static void Main()
+    {
+        //Behave just like Map Function
+        string[] inputs = Console.ReadLine().Split(" ");
+        List<string> list = new List<string>(inputs);
+        List<int> IntegerList= list.Select(x => Convert.ToInt32(x)).ToList();
+
+        int count = 0;
+        foreach(int x in IntegerList)
+        {
+            if(x >= 10)
+            {
+                count++;
+            }
+        }  
+        Console.WriteLine(count);
+
+    }
+}
